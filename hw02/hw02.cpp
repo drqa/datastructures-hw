27df089540f77e6cb66dffb47746553efe738a54
Daniel Ramirez <77579481+drqa@users.noreply.github.com>
2021-11-13 10:14:10 +0300

    Update hw02.cpp

--- hw02/hw02.cpp
+++ hw02/hw02.cpp
@@ -5,11 +5,13 @@
 
 using namespace std;
 
+//Warrior struct with a name and strength
 struct Warrior {
     string name;
     int strength;
 };
 
+//function prototypes
 void createWarrior(string name, int strength, vector<Warrior>& warriors);
 void doBattle(Warrior& warrior1, Warrior& warrior2);
 Warrior& searchWarriors(string name, vector<Warrior>& warriors);
@@ -29,22 +31,28 @@ int main(){
     
     while(file >> current){
 
+        //if the Warrior command is found, create a new Warrior and add him to the vector
         if(current == "Warrior"){
             file >> warriorName >> warriorStrength;
             createWarrior(warriorName, warriorStrength, warriors);
         }
+        //if the Battle command is found, first search the vector for the given Warriors
+        //If found, have those Warriors engage in glorious battle
         else if(current == "Battle"){
             file >> warriorName1 >> warriorName2;
             Warrior& warrior1 = searchWarriors(warriorName1, warriors);
             Warrior& warrior2 = searchWarriors(warriorName2, warriors);
             doBattle(warrior1, warrior2);
         }
+        //Status is our last and only remaining command, thus if we reach this far we know the command is Status
+        //display each Warriors current status
         else{
             status(warriors);
         }
     }
 }
 
+//Create an new Warrior and push it to the vector
 void createWarrior(string name, int strength, vector<Warrior>& warriors){
     Warrior warrior{name, strength};
     warriors.push_back(warrior);
@@ -54,6 +62,7 @@ void doBattle(Warrior& warrior1, Warrior& warrior2){
     
     cout << warrior1.name << " battles " << warrior2.name << endl;
     
+    //Check if either one or both warriors are already dead
     if(warrior1.strength == 0 && warrior2.strength == 0){
         cout << "Oh, NO! They're both dead! Yuck!" << endl;
     }
@@ -63,6 +72,8 @@ void doBattle(Warrior& warrior1, Warrior& warrior2){
     else if(warrior2.strength == 0){
         cout << "He's dead, " << warrior1.name << endl;
     }
+    //Compare strengths and calculate results of battle
+    //Set losing warriors strength to 0 to avoid negative ints
     else if(warrior1.strength == warrior2.strength){
             cout << "Mutual Annihilation: " << warrior1.name << " and " << warrior2.name << " die at each other's hands" << endl;
             warrior1.strength = 0;
@@ -83,18 +94,23 @@ void doBattle(Warrior& warrior1, Warrior& warrior2){
 Warrior& searchWarriors(string name, vector<Warrior>& warriors){
     //This is my great shame
     //I have to return a reference to a Warrior struct, but I've no clue how to do that if no match is found
-    //I look fotward to learning why this is dumb and how to do it better
+    //I look forward to learning why this is dumb and how to do it better
     Warrior nobody{"", 0};
+
+    //search the vector of Warriors for a Warrior with a given name
+    //once found return a reference to that Warrior
     for(Warrior& i : warriors){
         if(i.name == name){
             return(i);
         }
     }
 
+    //im a dumb guy
     Warrior& nobodyRef = nobody;
     return(nobodyRef);
 }
 
+//Iterate through the vector and display the names and strengths of each warrior within it
 void status(vector<Warrior>& warriors){
     cout << "There are: " << warriors.size() << " warriors" << endl;
     for(size_t i = 0; i < warriors.size(); i++){
